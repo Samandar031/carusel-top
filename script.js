78af2a19bd131fabe095fd97a490521a9fe9e382
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-03-23 14:49:17 +0500

    nvp tayyor

--- script.js
+++ script.js
@@ -1,40 +1,66 @@
-let imgs = document.getElementById("imgs");
-let left = document.getElementById("left");
-let right = document.getElementById("right");
+// let imgs = document.getElementById("imgs");
+// let left = document.getElementById("left");
+// let right = document.getElementById("right");
 
-let img = document.querySelectorAll("img");
+// let img = document.querySelectorAll("img");
 
-let inx = 0;
+// let inx = 0;
 
-function changeImage() {
-  if (inx > img.length - 1) {
-    inx = 0;
-  } else if (inx < 0) {
-    inx = img.length - 1;
-  }
-  imgs.style.transform = `translateX(${-inx * 500}px)`;
+// function changeImage() {
+//   if (inx > img.length - 1) {
+//     inx = 0;
+//   } else if (inx < 0) {
+//     inx = img.length - 1;
+//   }
+//   imgs.style.transform = `translateX(${-inx * 500}px)`;
+// }
+
+// right.addEventListener("click", function chang() {
+//   inx++;
+//   changeImage();
+// });
+
+// left.addEventListener("click", function chang() {
+//   inx--;
+//   resetIn();
+//   changeImage();
+// });
+
+// let int = setInterval(run, 2000);
+
+// function run() {
+//   inx++;
+//   changeImage();
+// }
+
+// function resetIn() {
+//   clearInterval(int);
+
+//   int = setInterval(run, 2000);
+// }
+
+let tepa = document.querySelector(".tepa");
+let past = document.querySelector(".pas");
+
+let img1 = document.querySelector(".one-box");
+let img2 = document.querySelector(".two-box");
+
+let a = 0;
+let b = 0;
+
+function ball() {
+  img1.style.transform = `translateY(${a * 1000}px)`;
+  img2.style.transform = `translateY(${-b * 1000}px)`;
 }
 
-right.addEventListener("click", function chang() {
-  inx++;
-  changeImage();
+tepa.addEventListener("click", function () {
+  a++;
+  b++;
+  ball();
 });
 
-left.addEventListener("click", function chang() {
-  inx--;
-  resetIn();
-  changeImage();
+past.addEventListener("click", function () {
+  a--;
+  b--;
+  ball();
 });
-
-let int = setInterval(run, 2000);
-
-function run() {
-  inx++;
-  changeImage();
-}
-
-function resetIn() {
-  clearInterval(int);
-
-  int = setInterval(run, 2000);
-}
